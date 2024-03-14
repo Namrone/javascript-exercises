c30eeba6479013ed0b39df4652856c2f8c7e264f
Namrone <151798569+Namrone@users.noreply.github.com>
2024-03-14 13:06:03 -0700

    Added javaScript to add words to a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(word,count) {
+    let phrase = '';
+    if(count < 0) return 'ERROR';
+    for(count; count > 0; count--) phrase += word;
+    return phrase;
 };
 
 // Do not edit below this line
