21a068fbb249aaccd686d517d5aaaf94cc07fb39
Namrone <151798569+Namrone@users.noreply.github.com>
2024-03-15 11:08:07 -0700

    Added javaScript to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordLength = word.length;
+    let reverseWord = [];
+    for(let count = 0; count < word.length; count++){
+        reverseWord[count] = word[wordLength -1];
+        wordLength--;
+    }
+    return reverseWord.join('');
 };
 
 // Do not edit below this line
