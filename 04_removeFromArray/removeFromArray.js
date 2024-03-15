2f4f2670543af112a54fefccc9013afad4e38451
Namrone <151798569+Namrone@users.noreply.github.com>
2024-03-15 14:06:02 -0700

    Added javaScript to remove items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function removeFromArray(array, ...numbers) {
+    for(let counts = 0; counts < numbers.length; counts++){
+        while(array.includes(numbers[counts]) == true){
+            let position = array.indexOf(numbers[counts]);
+            array.splice(position,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
