6e6b633aeb4334bd514a9f5df505763f31974b61
Namrone <151798569+Namrone@users.noreply.github.com>
2024-03-15 19:30:13 -0700

    Added javaScript to add sums of integers, but doesn't account for non integers yet

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function sumAll(num1, num2){
+    let sum = 0;
+    if(num1 > 0 && num2 > 0){
+        for(let count = Math.min(num1,num2); count <= Math.max(num1,num2); count++){
+            sum += count;
+        }
+        return sum;
+    }
+    else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
