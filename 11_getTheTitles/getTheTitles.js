fa97fbadff1aad56e3f1a3a4997b922dda7b6507
Namrone <151798569+Namrone@users.noreply.github.com>
2024-04-17 15:26:41 -0700

    Added javaScript to return book Titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+    const titles = book.map(books => books.title);
+    return titles;
 };
 
 // Do not edit below this line
