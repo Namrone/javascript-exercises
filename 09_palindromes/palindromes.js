5271519b0a3b3ff765c168cba0393c9a29efb1e6
Namrone <151798569+Namrone@users.noreply.github.com>
2024-04-17 12:15:58 -0700

    Added javaScript to complete the task and check if strings are palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const newStr = words.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase();
+    return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
