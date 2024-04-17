b6a90b31e4a4a3cf79c8fc45dea54e0201ef0af2
Namrone <151798569+Namrone@users.noreply.github.com>
2024-04-17 16:35:14 -0700

    Added javaScript to complete tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+    const currentDate = new Date();
+    const oldestName = person.map(function findAge(person){
+        if(typeof person.yearOfDeath === 'undefined'){person.yearOfDeath = currentDate.getFullYear()};
+        let age = person.yearOfDeath - person.yearOfBirth;
+        return age;
+    });
+    let index = 0;
+    let oldestPerson = oldestName[0];
+    for(let i = 0; i < oldestName.length; i++){
+        if(oldestPerson < oldestName[i+1]){
+            oldestPerson = oldestName[i+1];
+            index = i + 1;
+        }
+    }
+    return person[index];
 };
 
 // Do not edit below this line
