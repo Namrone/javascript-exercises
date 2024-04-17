4112d7d7ba11f493a9ecfc3f7cfad298eb4d70cd
Namrone <151798569+Namrone@users.noreply.github.com>
2024-04-17 10:30:47 -0700

    Added javaScript to complete the exercise with no errors

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const addAll = arr.reduce((total,currentItem) => total + currentItem,0);
+  return addAll;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const multAll = arr.reduce((total,currentItem) => total * currentItem,1);
+  return multAll;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let total = 1;
+  for(let i = 0; i < num2; i++){
+    total *= num1;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for(num; num > 0; num--){
+    total *= num;
+  }
+  return total;
 };
 
 // Do not edit below this line
