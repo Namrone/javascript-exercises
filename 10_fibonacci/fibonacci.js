80bb7ba3cdc89df06bb52e7d7fcc897cb6688603
Namrone <151798569+Namrone@users.noreply.github.com>
2024-04-17 14:24:19 -0700

    Added javaScript to complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = 0;
+    let fibLast = 1;
+    let fibSecond = 0;
+    console.log(typeof(num));
+    if(num >= 0){
+        if(num == 0){return 0;}
+        else if(num == 1){return 1;}
+        for(num; num > 1; num--){
+            fib = fibLast + fibSecond;
+            fibSecond = fibLast;
+            fibLast = fib;
+        }
+        return fib;
+    }
+    else
+        return 'OOPS';
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
